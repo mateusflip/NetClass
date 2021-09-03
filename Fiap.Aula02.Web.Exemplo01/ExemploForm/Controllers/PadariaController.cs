3daae2107f4c1e56fd89f5d29a1b69f81650a692
Mateus Ramos <mateus.ramos1337@gmail.com>
2021-09-02 22:58:47 -0300

    Formulário com métodos nativos do ASPNET

---
+++ Fiap.Aula02.Web.Exemplo01/ExemploForm/Controllers/PadariaController.cs
@@ -0,0 +1,36 @@
+﻿using ExemploForm.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploForm.Controllers
+{
+    public class PadariaController : Controller
+    {
+
+        private static List<Padaria> _banco = new List<Padaria>();
+        public IActionResult Index()
+        {
+            return View(_banco);
+        }
+        [HttpGet]//Abrir a página de cadastro
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        //Criar o POST para cadastrar na lista e exibir uma mensagem de sucesso
+        [HttpPost]
+        public IActionResult Cadastrar(Padaria padaria)
+        {
+            _banco.Add(padaria);
+
+            TempData["msg"] = "Padaria Registrada!";
+
+            return RedirectToAction("cadastrar");
+        }
+    
+    
+    }
+}
---
+++ Fiap.Aula02.Web.Exemplo01/ExemploForm/Models/Padaria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploForm.Models
+{
+    public class Padaria
+    {
+        public int Codigo { get; set; }
+
+        public string Nome { get; set; }
+        [Display(Name ="Disponível")]
+        public bool Disponivel { get; set; }
+        [DataType(DataType.Date), Display(Name ="Data de Abertura")]
+        public DateTime DataAbertura { get; set; }
+        public decimal Faturamento { get; set; }
+    }
+}
