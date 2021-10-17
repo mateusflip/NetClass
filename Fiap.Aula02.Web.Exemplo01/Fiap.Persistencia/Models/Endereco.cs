a3a26aea29579447b2e2415193b8ea0a17868199
Mateus Ramos <mateus.ramos1337@gmail.com>
2021-10-17 00:06:07 -0300

    Model Endereco

---
+++ Fiap.Aula02.Web.Exemplo01/Fiap.Persistencia/Models/Endereco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Persistencia.Models
+{
+    [Table("Tbl_Endereco")]
+    public class Endereco
+    {
+        public int EnderecoId { get; set; }
+        
+        public string Logradouro { get; set; }
+        [Required, MaxLength(9)]
+        public string cep { get; set; }
+
+    }
+}
